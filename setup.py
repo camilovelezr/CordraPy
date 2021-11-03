81a84e4a0d17897d2c0e697ce0595d6a505d6802
camilovelezr <camilov61@gmail.com>
2021-11-03 12:20:34 -0400

    Bump version 0.4

--- setup.py
+++ setup.py
@@ -14,9 +14,9 @@ PACKAGE_NAME = "CordraPy"
 
 setup(
     name=PACKAGE_NAME,
-    version='0.3.2',
+    version='0.4',
     description='Python client interface to a cordra instance',
-    author='Zachary Trautt, Faical Yannick Congo, Sven Voigt',
+    author='Zachary Trautt, Faical Yannick Congo, Sven Voigt, Camilo Velez',
     author_email='zachary.trautt@nist.gov',
     include_package_data=True,
     install_requires=fetch_requirements(),
